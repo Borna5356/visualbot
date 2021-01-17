3022122a934c335fb261e45f2d5931fd49e43695
Borna5356 <bornakevin1@gmail.com>
2021-01-17 01:52:45 -0500

    bot is able to recognise what color to click

--- visual_bot.py
+++ visual_bot.py
@@ -1,6 +1,6 @@
 import pyautogui
 import numpy
-from PIL import ImageGrab, ImageOps
+from PIL import ImageGrab, ImageOps, Image
 import time
 
 class Coordinates:
@@ -13,6 +13,11 @@ class Coordinates:
     (200, 400), (300, 400), (450, 400), (200, 540),
     (300, 540), (450, 540)]
 
+    #board x (143, 522)
+    #board y (208, 587)
+    #screen x (0, 1365)
+    #screen y (0, 767)
+
 def start_game():
     """
     Moves the mouse to the coordinates
@@ -20,6 +25,15 @@ def start_game():
 
     """
     pyautogui.click(Coordinates.start_button)
+
+def get_screen():
+    """
+    Get a screenshot of the full screen
+    
+    """
+    screen = (0, 0, 1365, 767)
+    image = ImageGrab.grab(screen)
+    return image
 
 def image_grab(square):
     """
@@ -37,19 +51,21 @@ def click_square(square):
     pyautogui.click(square)
 
 def main():
+    selected_squares = []
     start_game()
     time.sleep(.38)
-    selected_squares = []
     start = time.perf_counter()
     for square in Coordinates.level1_squares:
+        print(image_grab(square))
         if (image_grab(square) == 355):
             selected_squares.append(square)
     end = time.perf_counter()
     print(end - start)
-
     time.sleep(1)
     for square in selected_squares:
         click_square(square)
+    image = get_screen()
+
     
 
 main()
